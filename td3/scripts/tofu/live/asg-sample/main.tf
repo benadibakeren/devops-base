c08657aa36cb3d6eff922e2dc138b38640fa8fff
Keren Benadiba <keren.benadiba@edu.esiee.fr>
2026-01-07 15:09:29 +0100

    Fix ASG module source and configuration

--- td3/scripts/tofu/live/asg-sample/main.tf
+++ td3/scripts/tofu/live/asg-sample/main.tf
@@ -1,13 +1,12 @@
 module "asg" {
-  source = "git::https://github.com/benadibakeren/devops-base.git//td3/tofu/modules/asg"
+  source = "git::https://github.com/benadibakeren/devops-base.git//td3/scripts/tofu/modules/asg"
 
   name              = "sample-app-asg"
   ami_id            = "ami-0c960c064f6b4c8fb"
+  instance_type     = "t2.micro"
   user_data         = filebase64("${path.module}/user-data.sh")
   app_http_port     = 8080
-
-  instance_type     = "t2.micro"
   min_size          = 1
-  max_size          = 10
-  desired_capacity  = 3
+  max_size          = 3
+  desired_capacity  = 1
 }
